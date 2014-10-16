1dda6a4a5f45da92af04ebd99ba3cdb79def787c
huicpc0215 <huicpc0215@gmail.com>
2014-10-16 15:10:44 +0800

    hihocoder problem 1000

---
+++ hihocoder/1000/sol.cpp
@@ -0,0 +1,30 @@
+/*=============================================================================
+#     FileName: sol.cpp
+#         Desc: solution for problem 1000
+#       Author: WenShi(huicpc0215)
+#        Email: huicpc0215@gmail.com
+#     HomePage: huicpc0215.uni.me
+#      Version: 0.0.1
+#   LastChange: 2014-10-16 15:00:06
+#      History:
+=============================================================================*/
+
+/*
+ * solution:
+ * read a and b
+ * output a+b
+ */
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    while(cin>>a>>b)
+    {
+        cout<<a+b<<endl;
+    }
+    return 0;
+}
