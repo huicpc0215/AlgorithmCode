c8d1f2948a6c90e9266d53b379b2c4f33fb2f975
huicpc0215 <huicpc0215@gmail.com>
2014-10-18 19:52:38 +0800

    update solution for leetcode Longest Palindromic Substring

---
+++ leetcode/Longest-Palindromic-Substring/sol.cpp
@@ -0,0 +1,21 @@
+/*=============================================================================
+#     FileName: sol.cpp
+#         Desc: program for Longest Palindromic Substring
+#       Author: WenShi(huicpc0215)
+#        Email: huicpc0215@gmail.com
+#     HomePage: https://github.com/huicpc0215
+#      Version: 0.0.1
+#   LastChange: 2014-10-18 18:46:19
+#      History:
+=============================================================================*/
+
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<cmath>
+using namespace std;
+int main()
+{
+
+    return 0;
+}
