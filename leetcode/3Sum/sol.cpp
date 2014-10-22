2a8596522897b290f0973a4dfb11d34fd58e781e
huicpc0215 <huicpc0215@gmail.com>
2014-10-22 16:57:53 +0800

    mk base for 3 sum and k sum

---
+++ leetcode/3Sum/sol.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+using namespace std;
+class Solution{
+    public:
+    vector< vector<int> > ksum(vector<int> &num){
+
+
+
+    }
+
+};
+
+int main(){
+    int n;
+
+    return 0;
+}
