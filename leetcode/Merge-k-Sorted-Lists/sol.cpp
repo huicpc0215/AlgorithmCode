16a29edf4471432a8a32e37394c9deedccf22daa
huicpc0215 <huicpc0215@gmail.com>
2014-10-25 20:11:47 +0800

    create Merge k Sorted Lists

---
+++ leetcode/Merge-k-Sorted-Lists/sol.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<vector>
+using namespace std;
+struct ListNode{
+    int val;
+    ListNode *next;
+    ListNode(int x):val(x),next(NULL){}
+};
+class Solution{
+    public:
+    ListNode *mergeKLists(vector<ListNode *> &lists){
+
+    }
+
+}
+int main(){
+    return 0;
+}
