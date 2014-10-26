bac58ff3ff4f61d2ad7a1852ff4931b748b9b989
huicpc0215 <huicpc0215@gmail.com>
2014-10-26 23:57:01 +0800

    update for leetcode Merge k Sorted Lists

--- leetcode/Merge-k-Sorted-Lists/sol.cpp
+++ leetcode/Merge-k-Sorted-Lists/sol.cpp
@@ -1,3 +1,13 @@
+/*=============================================================================
+#     FileName: sol.cpp
+#         Desc: program for leetcode Merge k Sorted Lists
+#       Author: WenShi(huicpc0215)
+#        Email: huicpc0215@gmail.com
+#     HomePage: https://github.com/huicpc0215
+#      Version: 0.0.1
+#   LastChange: 2014-10-26 23:56:28
+#      History:
+=============================================================================*/
 #include<iostream>
 #include<cstdio>
 #include<cstring>
