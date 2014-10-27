669f3f374c76183052fc9a1401ba41148101f3ed
huicpc0215 <huicpc0215@gmail.com>
2014-10-27 23:46:31 +0800

    create solution for Swap Nodes in Pairs

---
+++ leetcode/Swap-Nodes-in-Pairs/sol.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cstdio>
+#Include<cstring>
+#Include<cmath>
+using namespace std;
+class Solution{
+    public:
+    ListNode *swapPairs(ListNode* head){
+
+    }
+
+
+};
+
+int main(){
+
+    return 0;
+}
