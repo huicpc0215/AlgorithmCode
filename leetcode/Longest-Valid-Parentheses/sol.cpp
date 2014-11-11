359542e87b25f2876115f2f0274fca648c7ff660
huicpc0215 <huicpc0215@gmail.com>
2014-11-11 20:16:49 +0800

    update solution for leetcode Longest Valid Parenttheses

--- leetcode/Longest-Valid-Parentheses/sol.cpp
+++ leetcode/Longest-Valid-Parentheses/sol.cpp
@@ -1,35 +1,56 @@
+/*=============================================================================
+#     FileName: sol.cpp
+#         Desc: program for leetcode Longest Valid Parentheses
+#       Author: WenShi(huicpc0215)
+#        Email: huicpc0215@gmail.com
+#     HomePage: https://github.com/huicpc0215
+#      Version: 0.0.1
+#   LastChange: 2014-11-11 20:11:46
+#      History:
+=============================================================================*/
 #include<iostream>
 #include<cstring>
 #include<cstdio>
 using namespace std;
 class Solution{
     public:
-    int stksz;
+    int stk[100000],par[100000],stksz;
     int longestValidParentheses(string s){
+        int res=0,tmpans=0;stksz=0;
         int sz=s.length();
-        int ans=0,tmpans=0;
-        stksz=0;
         for(int i=0;i<sz;i++){
-            if(s[i]=='(')
-                stksz++;
-            else {
-                if(stksz){
-                    stksz--;
-                    tmpans+=2;
+            if( s[i]=='(' ){
+                stk[stksz++]=i;
+                par[i]=-1;
+            }
+            else{
+                if( stksz==0 ){
+                    par[i]=-1;
                 }
                 else {
-                    tmpans=0;
-                    stksz=0;
+                    par[i]=stk[stksz-1];
+                    par[ stk[stksz-1] ]=i;
+                    stksz--;
                 }
-                ans=max(ans,tmpans);
             }
         }
-        return ans;
+        for(int i=0;i<sz;){
+            if(par[i]!=-1){
+                tmpans+=par[i]-i+1;
+                i=par[i]+1;
+                res=max(res,tmpans);
+            }
+            else {
+                tmpans=0;
+                i++;
+            }
+        }
+        return res;
     }
 };
 int main(){
     Solution s;
-    string st1="(()";
+    string st1="()(()";
     cout<<s.longestValidParentheses(st1)<<endl;
     string st2=")()()";
     cout<<s.longestValidParentheses(st2)<<endl;
