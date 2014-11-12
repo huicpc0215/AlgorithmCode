e5e42cc6692e88f658f7a4a128cc7baaea82e6d3
huicpc0215 <huicpc0215@gmail.com>
2014-11-12 23:55:19 +0800

    add solution to Leetcode Valid Sudoku

---
+++ leetcode/Valid-Sudoku/sol.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<cmath>
+#include<vector>
+using namespace std;
+class Solution{
+    public:
+    bool isValidSudoku(vector< vector<char> > &board){
+        bool f[10]={false};
+        // colume
+        for(int i=0;i<9;i++){
+
+        }
+        // row
+        for(int i=0;i<9;i++){
+
+        }
+        // block
+        for(int i=0;i<9;i+=3){
+            for(int j=0;j<9;j+=3){
+
+            }
+        }
+        return true;
+    }
+};
+int main(){
+
+}
+
