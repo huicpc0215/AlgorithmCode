b959cc0df8edb9a62a00a8006ca5a4239925bdc7
huicpc0215 <huicpc0215@gmail.com>
2014-11-15 21:50:52 +0800

    update Count and Say

---
+++ leetcode/Count-and-Say/sol.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<vector>
+using namespace std;
+class Solution{
+    public:
+    string countAndSay(int n){
+
+    }
+};
+int main(){
+
+
+
+}
+
