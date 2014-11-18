7890c87b79384dc704f9a3ccdb97a6e92e27625b
huicpc0215 <huicpc0215@gmail.com>
2014-11-18 21:41:24 +0800

    add problem Combination Sum

---
+++ leetcode/Combination-Sum/sol.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<cmath>
+#include<vector>
+using namespace std;
+
+class Solution {
+    public:
+    vector< vector<int> > combinationSum(vector<int> &candidates, int target){
+
+    }
+
+};
+int main(){
+
+    return 0;
+}
