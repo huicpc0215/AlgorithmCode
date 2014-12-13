3e4ee622ba8db98f8d1fed221a5c024088998145
huicpc0215 <huicpc0215@gmail.com>
2014-12-13 11:55:26 +0800

    update solution to leetcode Wildcard-Matching

---
+++ leetcode/Wildcard-Matching/sol.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+using namespace std;
+class Solution{
+    public:
+    bool isMatch(const char *s,const char *p){
+        const char *star=NULL;
+        const char *sPos=s;
+        while( *s ){
+            if( *p=='?'||*p==*s){
+                p++;s++;
+            }
+            else if( *p=='*' ){
+                star=p++;
+                sPos=s;
+            }
+            else if( star ){
+                p=star+1;
+                s=++sPos;
+            }
+            else return false;
+        }
+        while(*p=='*')p++;
+        return *p=='\0';
+    }
+};
+int main(){
+    Solution s;
+    printf("%s\n",s.isMatch("abc","*")?"YES":"NO");
+}
