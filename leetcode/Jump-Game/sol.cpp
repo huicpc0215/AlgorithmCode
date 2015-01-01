1e75b3b94378f2628a9e21fac798eeafe1220eb3
huicpc0215 <huicpc0215@gmail.com>
2015-01-01 11:05:09 +0800

    update solution to leetcode Jump-Game

---
+++ leetcode/Jump-Game/sol.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<cstdio>
+#include<cmath>
+#include<cstring>
+#include<queue>
+using namespace std;
+class Solution{
+public:
+    bool visit[100000];
+    queue<int> q;
+    int minpos;
+    bool canJump(int A[],int n){
+	    minpos=0;
+	    for(int i=0;i<n;i++)
+            visit[i]=false;
+        visit[0]=true;
+        q.push(0);
+        while(!q.empty()){
+            int t=q.front();
+            q.pop();
+            int maxpos=min(n-1,t+A[t]);
+            for(int i=max(minpos+1,t+1);i<=maxpos;i++){
+                if(i<n&&!visit[i]){
+                    q.push(i);
+                    visit[i]=true;
+                }
+            }
+	        minpos=max(minpos,t+A[t]);
+        }
+        return visit[n-1];
+    }
+};
+int main(){
+    int A[]={2,3,1,1,4};
+    Solution s;
+    printf("first = %d\n",s.canJump(A,5));
+    int B[]={3,2,1,0,4};
+    printf("second = %d\n",s.canJump(B,5));
+    return 0;
+}
