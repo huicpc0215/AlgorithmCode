54e234e5232add8a8bdd917b2cc90afb7ed090c4
huicpc0215 <huicpc0215@gmail.com>
2015-04-12 13:18:25 +0800

    update multiply Strings

--- Multiply-Strings/Solution.java
+++ Multiply-Strings/Solution.java
@@ -12,9 +12,20 @@
 import java.util.*;
 import java.math.*;
 public class Solution{
-    public String multiply(String num1,String num2){
+    public static String multiply(String num1,String num2){
         BigInteger a=new BigInteger(num1);
         BigInteger b=new BigInteger(num2);
         return (a.multiply(b).toString());
+    }
+    public static void main(String args[]){
+        Scanner cin=new Scanner(System.in);
+        String a=new String();
+        String b=new String();
+        while(cin.hasNext()){
+            a=cin.next();
+            b=cin.next();
+            System.out.println("a="+a+" b="+b+" ans="+(multiply(a,b)));
+        }
+        return ;
     }
 }
