05a5658c7a43663160cac6d3d8651c898f19049d
huicpc0215 <huicpc0215@gmail.com>
2015-05-23 19:06:49 +0800

    update hihocoder 1038

--- hihocoder/1038/sol.cpp
+++ hihocoder/1038/sol.cpp
@@ -9,3 +9,28 @@
 #      History:
 =============================================================================*/
 
+#include<iostream>
+#include<cstdio>
+#include<limits.h>
+using namespace std;
+#define MAXM 100010
+int n,m,w,v;
+int dp[MAXM];
+int main(){
+    scanf("%d%d",&n,&m);
+    for(int i=0;i<=m;i++){
+        dp[i]=0;
+    }
+    for(int i=0;i<n;i++){
+        scanf("%d%d",&w,&v);
+        for(int j=m;j>=w;j--){
+            dp[j]=max(dp[j],dp[j-w]+v);
+        }
+    }
+    int ans=0;
+    for(int i=0;i<=m;i++){
+        ans=max(ans,dp[i]);
+    }
+    printf("%d\n",ans);
+    return 0;
+}
